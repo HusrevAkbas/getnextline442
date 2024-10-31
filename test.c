c58f0b5badd8e2b32afa52a15ade0acc4848081d
Husrev Akbas <huakbas@c2r3p1.42vienna.com>
2024-10-31 14:53:20 +0100

    new tests added

--- test.c
+++ test.c
@@ -3,8 +3,9 @@
 
 int main()
 {
-	int	fd = 5;// open("file", O_RDONLY);
-	char *str = "";
+	/*TEST MANDATORY*/
+	//int	fd = open("file", O_RDONLY);
+	//char *str = "";
 	//get_next_line(NULL);
 	
 	//if (fd != -1)
@@ -17,13 +18,45 @@ int main()
 	//	}
 	//}
 	
+	/*TEST USER INPUT*/
 	//while ((str = get_next_line(0)))
 	//{
 	//	printf("%s", str);
 	//}
-	str = get_next_line(fd);
+	
+	/*TEST MITIPLE FILES*/
+	int fda = open("filea", O_RDONLY);
+	int fdb = open("fileb", O_RDONLY);
+	int fdc = open("filec", O_RDONLY);
+	
+	char *str;
+	
+	str = get_next_line(fda);
 	printf("%s", str);
-	free(str);
+	
+	str = get_next_line(fdb);
+	printf("%s", str);
+	
+	str = get_next_line(fdc);
+	printf("%s", str);
+	
+	str = get_next_line(fdc);
+	printf("%s", str);
+	
+	str = get_next_line(fda);
+	printf("%s", str);
+	
+	str = get_next_line(fdb);
+	printf("%s", str);
+	
+	str = get_next_line(fda);
+	printf("%s", str);
+	
+	str = get_next_line(fdc);
+	printf("%s", str);
+	
+	str = get_next_line(fdb);
+	printf("%s", str);
 	
 	return (0);
 }
